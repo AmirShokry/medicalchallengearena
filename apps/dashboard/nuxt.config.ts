f16196bfdab04e378c7771315175b5c6e777cff2
Amir Shokry <amirshokryabozaid@gmail.com>
2025-07-04 20:29:00 +0300

    Added shadcn vue & tailwind

--- apps/dashboard/nuxt.config.ts
+++ apps/dashboard/nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
+	modules: ["shadcn-nuxt"],
+	shadcn: {
+		prefix: "",
+		componentDir: "./components/ui",
+	},
 	vite: {
 		cacheDir: "../../node_modules/.vite/dashboard",
 	},
