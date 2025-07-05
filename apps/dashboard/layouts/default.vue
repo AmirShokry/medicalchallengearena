27b91a1753f7ee30d69b51fe5dda28d3ff68cf99
Amir Shokry <amirshokryabozaid@gmail.com>
2025-07-05 15:57:33 +0300

    Added a default layout

---
+++ apps/dashboard/layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { sidebarWidth, sidebarWidthMobile } from "~/components/ui/sidebar";
+</script>
+
+<template>
+	<SidebarProvider
+		:style="{
+			'--sidebar-width': sidebarWidth,
+			'--sidebar-width-mobile': sidebarWidthMobile,
+		}">
+		<SidebarsDefault collapsible="offcanvas" />
+		<SidebarInset>
+			<slot />
+		</SidebarInset>
+	</SidebarProvider>
+</template>
