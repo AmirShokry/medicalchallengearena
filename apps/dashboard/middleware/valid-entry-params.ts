40261f5751b9c7bb92cb19b86d1ef13e3e3af378
Amir Shokry <amirshokryabozaid@gmail.com>
2025-07-13 23:45:04 +0300

    Added before entry route middleware

---
+++ apps/dashboard/middleware/valid-entry-params.ts
@@ -0,0 +1,12 @@
+export default defineNuxtRouteMiddleware(async (to, _) => {
+	if (to.name === "entry-system-category") {
+		const { $trpc } = useNuxtApp();
+		const isValidSystemCategory =
+			await $trpc.common.isValidSystemCategory.query({
+				system: to.params.system,
+				category: to.params.category,
+			});
+		to.meta.sysTest = isValidSystemCategory;
+		if (!isValidSystemCategory) return navigateTo("/404");
+	}
+});
