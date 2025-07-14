9e12f9a845d36f59313a0a84949b78f591f2d932
Amir Shokry <amirshokryabozaid@gmail.com>
2025-07-14 23:43:18 +0300

    Clean up default route

--- apps/dashboard/pages/index.vue
+++ apps/dashboard/pages/index.vue
@@ -1,19 +1,16 @@
-<script setup lang="ts"></script>
 <template>
 	<header
 		class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
 		<div class="flex items-center gap-2 px-4">
 			<SidebarTrigger class="-ml-1" />
-			<Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
+			<Separator
+				orientation="vertical"
+				class="mr-2 data-[orientation=vertical]:h-4" />
 			<Breadcrumb>
 				<BreadcrumbList>
 					<BreadcrumbItem class="hidden md:block">
 						<BreadcrumbLink href="#"> Home </BreadcrumbLink>
 					</BreadcrumbItem>
-					<!-- <BreadcrumbSeparator class="hidden md:block" /> -->
-					<!-- <BreadcrumbItem>
-							<BreadcrumbPage>Data Fetching</BreadcrumbPage>
-						</BreadcrumbItem> -->
 				</BreadcrumbList>
 			</Breadcrumb>
 		</div>
