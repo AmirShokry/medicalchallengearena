9e4356f8862a533e1b025129fa8c3b325525c93a
Amir Shokry <amirshokryabozaid@gmail.com>
2025-07-29 21:20:04 +0300

    Altered database package to use shared utils package

--- packages/database/env.ts
+++ packages/database/env.ts
@@ -1,17 +1,17 @@
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
-import loadEnv from "./load-env";
+import { loadEnv } from "@package/utils";
 
-loadEnv();
+loadEnv("packages/database/.env");
 
 export const env = createEnv({
-	server: {
-		DB_USER: z.string().min(1),
-		DB_PASSWORD: z.string().min(1),
-		DB_NAME: z.string().min(1),
-		DB_HOST: z.string().min(1),
-		DB_PORT: z.coerce.number().min(1).max(65535),
-	},
-	runtimeEnv: process.env,
+  server: {
+    DB_USER: z.string().min(1),
+    DB_PASSWORD: z.string().min(1),
+    DB_NAME: z.string().min(1),
+    DB_HOST: z.string().min(1),
+    DB_PORT: z.coerce.number().min(1).max(65535),
+  },
+  runtimeEnv: process.env,
 });
--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -10,33 +10,31 @@ import * as schema from "./schema";
 import { env } from "../env";
 
 const pg = postgres(
-	`postgres://${env.DB_USER}:${env.DB_PASSWORD}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}`
+  `postgres://${env.DB_USER}:${env.DB_PASSWORD}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}`
 );
 
 const db = drizzle(pg, { schema }) as PostgresJsDatabase<typeof schema> & {
-	$client: postgres.Sql<{}>;
+  $client: postgres.Sql<{}>;
 } & {
-	table: {
-		[k in keyof typeof schema as (typeof schema)[k] extends PgTable
-			? k
-			: never]: (typeof schema)[k];
-	};
-	views: typeof schema.views;
+  table: {
+    [k in keyof typeof schema as (typeof schema)[k] extends PgTable
+      ? k
+      : never]: (typeof schema)[k];
+  };
+  views: typeof schema.views;
 };
 db.table = db._.fullSchema;
 db.views = schema.views;
 
 export async function checkConnection() {
-	console.log("Checking database connection");
-	await pg`SELECT 1`.catch((err) => {
-		console.error(err);
-		process.exit(1);
-	});
-	console.log("✅ Database connection established successfully.");
+  console.log("Checking database connection");
+  await pg`SELECT 1`.catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+  console.log("✅ Database connection established successfully.");
 }
 
 export { db };
 export * from "drizzle-orm";
-// export * from "./helpers";
-
 export * from "./schema";
